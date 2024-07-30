e3021b6ef69568539366566abf85804115d6a2ff
Stephanie Xue <stephaniexue3@gmail.com>
2024-07-29 22:42:50 -0700

    fix spacing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,11 +15,11 @@ function Card(props) {
                 <div className="card-badge">{badgeText}</div>
             }
             <img 
-                src={`../../public/images/${props.coverImg}`} 
+                src={`images/${props.coverImg}`} 
                 className="card-image" 
             />
             <div className="card-stats">
-                <img src="../images/star.png" className="card-star" />
+                <img src="/images/star.png" className="card-star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
